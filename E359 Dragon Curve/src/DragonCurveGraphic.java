0faf6d7811a2d895c7722f66ea23e742e0112f94
nucleusfox <bountyfaye@gmail.com>
2018-06-26 20:58:55 +0300

    Neat the code. Remove drafts.

--- E359 Dragon Curve/src/DragonCurveGraphic.java
+++ E359 Dragon Curve/src/DragonCurveGraphic.java
@@ -20,58 +20,6 @@ import java.util.Scanner;
 
 public class DragonCurveGraphic {
 
-    static class LineComponent extends JComponent {
-
-        ArrayList<Line2D.Double> lines;
-        Random random;
-
-        LineComponent(int width, int height) {
-            super();
-            setPreferredSize(new Dimension(width,height));
-            lines = new ArrayList<Line2D.Double>();
-            random = new Random();
-        }
-
-        public void addLine() {
-            int width = (int)getPreferredSize().getWidth();
-            int height = (int)getPreferredSize().getHeight();
-            Line2D.Double line = new Line2D.Double(
-                    random.nextInt(width),
-                    random.nextInt(height),
-                    random.nextInt(width),
-                    random.nextInt(height)
-            );
-            lines.add(line);
-            repaint();
-        }
-
-//        public void paintComponent(Graphics g) {
-//
-//            g.setColor(Color.white);
-//            g.fillRect(0, 0, getWidth(), getHeight());
-//            Dimension d = getPreferredSize();
-//
-//
-//            for (Line2D.Double line : lines) {
-//                int r = (int) (Math.random()*5);
-//                switch(r) {
-//                    case 1 : g.setColor(Color.yellow); break;
-//                    case 2 : g.setColor(Color.blue); break;
-//                    case 3 : g.setColor(Color.green); break;
-//                    case 4 : g.setColor(Color.red); break;
-//                    case 5 : g.setColor(Color.magenta); break;
-//                }
-//
-//                g.drawLine(
-//                        (int)line.getX1(),
-//                        (int)line.getY1(),
-//                        (int)line.getX2(),
-//                        (int)line.getY2()
-//                );
-//            }
-//        }
-    }
-
     static class Dragon extends JComponent {
         public final int BEND_STEP = 15;
         public ArrayList<Line2D.Double> lines;
