ee941843c1c6d937b0909e9b3bb555e4adb50366
nucleusfox <bountyfaye@gmail.com>
2018-07-07 23:07:22 +0300

    Added nice colors and scales.

--- E359 Dragon Curve/src/DragonCurveGraphic.java
+++ E359 Dragon Curve/src/DragonCurveGraphic.java
@@ -5,36 +5,73 @@
  *  Dragon Curve Graphic
  *  Accepts the number G and prints and draws the dragon curve Gth generation.
  ******************************************************************************/
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
+
+import java.awt.*;
 import java.awt.geom.Line2D;
 
-import javax.swing.JOptionPane;
-import javax.swing.JComponent;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
 
 public class DragonCurveGraphic {
+    private static  Color[] PALETTE = new Color[]{
+        new Color(119,9,167),
+//        new Color(119,9,167),
+        new Color(226,0,138),
+        new Color(234,3,14),
+        new Color(226,88,0),
+        new Color(102,203,111),
+        new Color(102,203,111),
+        new Color(134,203,106),
+        new Color(134,203,106),
+        new Color(134,203,106),
+        new Color(134,203,106),
+        new Color(134,203,106),
+        new Color(134,203,106),
+        new Color(148,221,11),
+        new Color(148,221,11),
+        new Color(182,221,7),
+        new Color(182,221,7),
+        new Color(221,208,13),
+        new Color(221,208,13),
+        new Color(221,195,8),
+//        new Color(221,195,8),
+        new Color(226,88,0),
+//        new Color(234,3,14),
+        new Color(226,0,138),
+        new Color(119,9,167)
+    };
 
     static class Dragon extends JComponent {
         public final int BEND_STEP = 15;
+        public double SIZE_COEFFICIENT = 1;
+        private int shiftX = 0;
+        private int shiftY = 0;
+
         public ArrayList<Line2D.Double> lines;
 
-        Dragon(int width, int height) {
+        Dragon(int width, int height, int generation) {
             super();
-            setPreferredSize(new Dimension(width,height));
+            if (generation > 10) SIZE_COEFFICIENT = 1.5;
+            if (generation > 12) SIZE_COEFFICIENT = 2;
+            if (generation > 14) {
+                SIZE_COEFFICIENT = 3;
+                shiftX += width/2;
+                shiftY += height*1.5;
+            }
+            if (generation > 17) SIZE_COEFFICIENT = 5;
+            int w = (int) Math.round(width*SIZE_COEFFICIENT);
+            int h = (int) Math.round(height*SIZE_COEFFICIENT);
+            setPreferredSize(new Dimension(w,h));
             lines = new ArrayList<>();
         }
 
 
         public void bear(short[] dragon, int startX, int startY) {
-            lines.add(new Line2D.Double(startX, startY, startX, startY-BEND_STEP));
-            int[] currentPoint = new int[]{startX, startY-BEND_STEP};
-            int[] currentVector = new int[]{0, -1};
+            lines.add(new Line2D.Double(startX+shiftX, startY+shiftY, startX+shiftX, startY-BEND_STEP/SIZE_COEFFICIENT+shiftY));
+            double[] currentPoint = new double[]{startX+shiftX, startY-BEND_STEP/SIZE_COEFFICIENT+shiftY};
+            double[] currentVector = new double[]{0, -1};
 
             for (short d : dragon) {
                 if (d == 1) currentVector = RotatingMatrix.rotateLeft(currentVector);
@@ -43,11 +80,11 @@ public class DragonCurveGraphic {
                 Line2D.Double line = new Line2D.Double(
                         currentPoint[0],
                         currentPoint[1],
-                        currentPoint[0] + currentVector[0]*BEND_STEP,
-                        currentPoint[1] + currentVector[1]*BEND_STEP
+                        currentPoint[0] + currentVector[0]*BEND_STEP/SIZE_COEFFICIENT,
+                        currentPoint[1] + currentVector[1]*BEND_STEP/SIZE_COEFFICIENT
                 );
-                currentPoint[0] += currentVector[0] *BEND_STEP;
-                currentPoint[1] += currentVector[1]*BEND_STEP;
+                currentPoint[0] += currentVector[0]*BEND_STEP/SIZE_COEFFICIENT;
+                currentPoint[1] += currentVector[1]*BEND_STEP/SIZE_COEFFICIENT;
 
                 lines.add(line);
                 repaint();
@@ -55,23 +92,22 @@ public class DragonCurveGraphic {
             }
         }
         public void paintComponent(Graphics g) {
-
             g.setColor(Color.white);
             g.fillRect(0, 0, getWidth(), getHeight());
             int color = 0;
             int colorLength = 0;
+            int index = 0;
+            int generation = 1;
 
             for (Line2D.Double line : lines) {
                 int r = color / 7;
-                switch(r) {
-                    case 0 : g.setColor(Color.yellow); break;
-                    case 1 : g.setColor(Color.orange); break;
-                    case 2 : g.setColor(Color.red); break;
-                    case 3 : g.setColor(Color.magenta); break;
-                    case 4 : g.setColor(Color.blue); break;
-                    case 5 : g.setColor(Color.cyan); break;
-                    case 6 : g.setColor(Color.green); break;
-                }
+                double k = generation / PALETTE.length;
+                double indexDouble = (double) index;
+                double i = indexDouble / k;
+                if (generation < PALETTE.length*2) i = 5;
+                if (i >= PALETTE.length) i = PALETTE.length-1;
+                System.out.print("i = " + i + " ");
+                g.setColor(PALETTE[(int)i]);
                 colorLength ++;
                 color += colorLength / 10;
                 colorLength = colorLength % 11;
@@ -84,6 +120,14 @@ public class DragonCurveGraphic {
                         (int)line.getX2(),
                         (int)line.getY2()
                 );
+
+                System.out.println("index = " + index + ", generation = " + generation);
+                index++;
+                if (index == generation) {
+                    generation *= 2;
+                    index = 0;
+                }
+
             }
         }
     }
@@ -97,12 +141,27 @@ public class DragonCurveGraphic {
         Runnable rd = new Runnable() {
             public void run() {
                 int width = 1600, height = 1000;
-                Dragon dragon = new Dragon(width, height);
+                Dragon dragon = new Dragon(width, height, G);
                 dragon.bear(dragonSequence, width*5/7, height*3/7);
 
-                JOptionPane.showMessageDialog(null, dragon);
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+                    return;
+                }
+
+                JFrame frame = new JFrame("Dragon Curve " + G);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLayout(new BorderLayout());
+                JScrollPane scrollPane = new JScrollPane(dragon);
+                frame.add(scrollPane);
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
             }
         };
+
+
         SwingUtilities.invokeLater(rd);
     }
 
--- E359 Dragon Curve/src/RotatingMatrix.java
+++ E359 Dragon Curve/src/RotatingMatrix.java
@@ -8,8 +8,8 @@
 import java.util.Arrays;
 
 public class RotatingMatrix {
-    private static int[][] L = new int[2][2];
-    private static int[][] R = new int[2][2];
+    private static double[][] L = new double[2][2];
+    private static double[][] R = new double[2][2];
 
     static {
         L[0][0] = 0;    L[0][1] = -1;
@@ -18,21 +18,21 @@ public class RotatingMatrix {
         R[1][0] = -1;   R[1][1] = 0;
     }
 
-    public static int[] rotateLeft(int[] vector) {
-        int[] r = new int[2];
+    public static double[] rotateLeft(double[] vector) {
+        double[] r = new double[2];
         r[0] = L[0][0] * vector[0] - L[0][1] * vector[1];
         r[1] = - L[1][0] * vector[0] + L[1][1] * vector[1];
         return r;
     }
 
-    public static int[] rotateRight(int[] vector) {
-        int[] r = new int[2];
+    public static double[] rotateRight(double[] vector) {
+        double[] r = new double[2];
         r[0] = R[0][0] * vector[0] - R[0][1] * vector[1];
         r[1] = - R[1][0] * vector[0] + R[1][1] * vector[1];
         return r;
     }
 
-    private static void printVector(int[] vector) {
+    private static void printVector(double[] vector) {
         System.out.println(Arrays.toString(vector));
     }
 }
